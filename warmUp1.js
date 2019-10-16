2ae906eefb2b0127bad2bcc4f4582be84668e8f8
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2019-10-16 10:37:11 +0200

    solved

--- warmUp1.js
+++ warmUp1.js
@@ -1,4 +1,8 @@
 // 1-Create a third variable called z, assign x + y to it, and display it.
 var x = 5;
 var y = 10;
+var z=x+y;
+console.log(z)
 // 2-calculate the area of a square using varaiables.
+var length=6;
+var squareArea=length*length;
