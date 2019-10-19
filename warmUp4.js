dc7e85a8b38234d1458fc1c580ea32a2142e5c3c
Omar Belguith <omar.belguith@medtech.tn>
2019-10-19 09:07:04 +0100

    WarmUp4

---
+++ warmUp4.js
@@ -0,0 +1,7 @@
+// 1-Write a function that takes a string as an input and returns the reverse of each letter 
+//followed by a number starting from zero(solve it using while loop).
+// example:    reverseStr('hello'); "o1l2l3e4h"
+
+
+// 2-write a function that takes array of strings and returns an array of the strings that have the same length
+// example :  ['hi','hello','welcome','hy'] == > ["hi","hy"]
