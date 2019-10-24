c986f9cdddd41236f7d52106c0f5e1ea594e72b8
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2019-10-24 10:46:55 +0200

    7th warmUp

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,24 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+/////////////////////////////////Q1
+function fullName(firstName, lastName) {
+	return firstName+" "+lastName;
+}
+fullName("Mahdi","ali");
+/////////////////////////////////Q2
+console.log(13%3===0)
+////////////////////////////////Q3
+function average(array) {
+	var sum=0
+	for(var i in array) {
+		sum+=array[i];
+	}
+	return sum/array.length;
+}
+var ages= [13,14,13,15,16,17,19,13,16,15];
+/////////////////////////////Q4
+function ageInSecondes(age) {
+	return age * 365 * 24 * 60 * 60;
+}
+ageInSecondes(30);
