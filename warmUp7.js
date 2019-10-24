d611742b022a6d5cfbe73792286b392341367912
Omar Belguith <omar.belguith@medtech.tn>
2019-10-24 09:28:25 +0100

    warmup7

---
+++ warmUp7.js
@@ -0,0 +1,6 @@
+// 1-using + operator combine your partner first and last name .
+// 2-find if the number 13 is a multiple of 3 or not.
+// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+// 4-calculate your age in seconds.
+
+// your code is here
