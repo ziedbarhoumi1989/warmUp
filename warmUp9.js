9c1126eab0a4c45d6e1938e592c821fc2645ea38
Omar Belguith <omar.belguith@medtech.tn>
2019-10-26 09:13:31 +0100

    warmUp9

---
+++ warmUp9.js
@@ -0,0 +1,10 @@
+// Create an HTML file
+// Create a CSS file
+// link the CSS file to the HTML file
+// The page should have a header, body and footer
+// Change the background color of the body to be blue
+// Insert Three images next to each other
+// Create Two buttons and use different font family for each button
+// Create an unordered list with three anchor tags in it
+// Place In the footer a copyright claimer
+// Have Fun
