5cb57434612b89622a9226017d8a15a148f2286c
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2019-10-29 12:56:13 +0100

    the 10th warm up

---
+++ ziedbarhoumi.js
@@ -0,0 +1,14 @@
+//alert("hello");
+//$('body').css('background','red');
+var value=$('input type:text').val();
+//console.log(value);
+//alert(value);
+$('button').on('click', function{
+	$(ul).append(<li></li>)
+	var value=$('input type:text').val();
+	$('li').text.call(value)
+	$('li').attr('class',class1)
+	
+
+})
+$('ul ').append('<li>value</li>')
