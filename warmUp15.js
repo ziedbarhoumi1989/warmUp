d30789e4b72f8c1f8b3634216753807cb8427f80
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2019-11-05 09:51:47 +0100

    warmup 15 not solved

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,31 @@
 //
 //
 // Note : you can`t use any built-in functions .
+function reverse(string) {
+	var arr=[]
+	var result=[]
+	var str=""
+	var reversed=""
+	for (var i=0;i<string.length;i++) {
+		arr[i]=string[i]
+
+
+	}
+	for (var i=arr.length-1;i<0;i--) {
+		end=i
+		var index=0;
+		if(arr[i]===" "){
+			start=i
+		}
+		for(var j=start+1;j<=end;j++) {
+			str += arr[j]
+		}
+		result[index]=str
+		str="";
+	}
+	for (var a=0; a<result.length-1;a++) {
+		reversed+= result[a]
+	}
+	reversed += result[result.length-1]
+	return reversed;
+}
