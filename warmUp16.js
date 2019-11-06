e8bd14a7f05bd8cfdad114b17166feec80b66f1f
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2019-11-06 09:39:59 +0100

    warmup16 done

--- warmUp16.js
+++ warmUp16.js
@@ -16,3 +16,32 @@
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+function instances(string) {
+	var obj={}
+
+	obj.b=0
+	obj.a=0
+	obj.l=0
+	obj.o=0
+	obj.n=0
+
+	for (var i=0; i<string.length;i++) {
+		for (var key in obj) {
+			if(string[i] === key) {
+				obj[key]=obj[key]+1
+			}
+		}
+	}
+
+	obj.l= Math.floor((obj.l)/2)
+	obj.o= Math.floor((obj.o)/2)
+	
+	var min=obj["b"]
+	for (key in obj) {
+		if(obj[key] <= min) {
+			min = obj[key]
+		}
+	}
+	return min
+	
+}
